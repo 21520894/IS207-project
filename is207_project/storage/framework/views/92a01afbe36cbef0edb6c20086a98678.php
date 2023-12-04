6dac6ddea12d5136dc9125101ee54e61a48d055c
Duy Hung <21520894@gm.uit.edu.vn>
2023-12-04 08:45:32 +0700

    Revert "recommit"
    This reverts commit 48a9c48ebd0075f5d780a85969f511c07f4b0e85.

--- is207_project/storage/framework/views/92a01afbe36cbef0edb6c20086a98678.php
+++
@@ -1,8 +0,0 @@
-<?php $__env->startSection('content'); ?>
-    <?php echo $__env->make('blocks/slider', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
-    <?php echo $__env->make('blocks/promotion', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
-    <?php echo $__env->make('blocks/about', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
-    <?php echo $__env->make('blocks/consult', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
-<?php $__env->stopSection(); ?>
-
-<?php echo $__env->make('layouts/clients', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH D:\subject_learning\is207\wamp64\www\IS207_project_git\is207_project\resources\views/clients/user/home.blade.php ENDPATH**/ ?>
